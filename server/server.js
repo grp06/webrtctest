3f3aa186e8bbea4942bd8cd7955fd837dd003596
George Pickett <gpickett00@gmail.com>
2020-03-29 19:33:41 -0400

    simplifuybe

--- server/server.js
+++ server/server.js
@@ -4,32 +4,79 @@ var io = require('socket.io')(server);
 
 server.listen(8080);
 
-io.on('connection', function (socket) {
-    socket.on('join', function (data) {
-        socket.join(data.roomId);
-        socket.room = data.roomId;
-        const sockets = io.of('/').in().adapter.rooms[data.roomId];
-        if(sockets.length===1){
-            socket.emit('init')
-        }else{
-            if (sockets.length===2){
-                io.to(data.roomId).emit('ready')
-            }else{
-                socket.room = null
-                socket.leave(data.roomId)
-                socket.emit('full')
-            }
-            
+io.on('connection', function(socket) {
+  socket.on('join', function (data) {
+    const sockets = io.of('/').in().adapter.rooms;
+    let myRoom
+    const joinRoom = () => {
+      for (const key in sockets) {
+        if (sockets.hasOwnProperty(key)) {
+          const room = sockets[key];
+          console.log('room = ', room)
+          if (key.length === 7 && room.length === 1) {
+            socket.join(key);
+            myRoom = key
+            console.log('joining an existing room ', key)
+            io.to(key).emit('welcomeMessage', {
+              waitingForPartner: false,
+              roomId: key,
+            })
+          }
         }
-    });
-    socket.on('signal', (data) => {
-        io.to(data.room).emit('desc', data.desc)        
+      }
+
+      if (!myRoom) {
+        myRoom = data.roomId
+        console.log('creating room with ID ', myRoom )
+        socket.join(myRoom);
+        io.to(myRoom).emit('welcomeMessage', {
+          waitingForPartner: true,
+          roomId: myRoom
+        })
+      }
+    }
+    joinRoom()
+
+    socket.on('chat', function(data) {
+      io.to(myRoom).emit('chat', data)
     })
-    socket.on('disconnect', () => {
-        const roomId = Object.keys(socket.adapter.rooms)[0]
-        if (socket.room){
-            io.to(socket.room).emit('disconnected')
-        }
-        
+
+    socket.on('typing', function(data) {
+      socket.broadcast.emit('typing', data)
     })
-});
+
+    // if (sockets.length===1) {
+    //   console.log('length = 1')
+    //   socket.emit('init')
+    // } else{
+    //   if (sockets.length===2){
+    //     console.log("sockets length 2")
+    //     io.to(data.roomId).emit('ready')
+    //   } else {
+    //     console.log("sockets not 1 or 2 ... I guess we leave here and emit full?")
+    //     socket.room = null
+    //     socket.leave(data.roomId)
+    //     socket.emit('full')
+    //   }
+    // }
+  });
+
+
+
+  // const mySocketId = socket.id
+  // listOfSocketIds.push(mySocketId)
+
+
+
+
+  // socket.on('disconnect', function(){
+  //   const index = listOfSocketIds.indexOf(mySocketId);
+  //   if (index > -1) {
+  //     listOfSocketIds.splice(index, 1);
+  //   }
+  // });
+
+
+
+})
+
